d4cd3b2fd930589733226b9b6e2226bd561067c7
Phherten <phigueraherten@gmail.com>
2022-05-19 09:21:28 +0000

    falta acbar

--- src/models.py
+++ src/models.py
@@ -8,6 +8,72 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
+class User(Base):
+    __tablename__="user"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    apellido = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+
+class Personaje(Base):
+    __tablename__="personaje"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    apellido = Column(String(250), nullable=False)
+    planeta = Column(String(250), nullable=False)
+    genero = Column(String(250), nullable=False)
+
+class Personaje_favorito(Base):
+    __tablename__="personaje_favorito"
+    id = Column(Integer, primary_key=True)
+    id_user = Column(Integer,ForeignKey('user.id'))
+    user = relationship(User)
+    id_personaje = Column(Integer,ForeignKey('personaje.id'))
+    personaje = relationship(Personaje)
+
+class Planeta(Base):
+    __tablename__= "planeta"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    poblacion = Column(Integer, nullable=False)
+    tamaño = Column(Integer, nullable=False)
+    clima = Column(String(250), nullable=False)
+
+
+class Planeta_favorito(Base):
+    __tablename__="planeta_favorito"
+    id = Column(Integer, primary_key=True)
+    id_user = Column(Integer,ForeignKey('user.id'))
+    user = relationship(User)
+    id_planeta = Column(Integer,ForeignKey('planeta.id'))
+    planeta = relationship(Planeta)
+
+
+class Nave(Base):
+    __tablename__:"nave"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    tamaño = Column(Integer, nullable=False)
+    tipo = Column(String(250), nullable=False)
+
+class Nave_favorito(Base):
+    __tablename__="nave_favorito"
+    id = Column(Integer, primary_key=True)
+    id_user = Column(Integer,ForeignKey('user.id'))
+    user = relationship(User)
+    id_planeta = Column(Integer,ForeignKey('nave.id'))
+    nave = relationship(Nave)
+
+
+
+
+
+
+    
+
+    
+
+
 
 
 """
@@ -32,5 +98,5 @@ class Address(Base):
     def to_dict(self):
         return {}
 """
-## Draw from SQLAlchemy base
+## Draw from SQLAlchemy Base
 render_er(Base, 'diagram.png')
